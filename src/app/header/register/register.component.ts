ce3749802fbaf4322b9feb3f649a6910a4f5652b
Lawanya98 <chithminilawanya@gmail.com>
2022-03-04 10:39:23 +0530

    Registration Form Validations Checked

--- src/app/header/register/register.component.ts
+++ src/app/header/register/register.component.ts
@@ -5,7 +5,7 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserServicesService } from '../../services/user-services.service';
 import { MustMatch } from '../../shared/must-match.validators';
 import { User } from '../../shared/user.model';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -15,6 +15,7 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
   emailRegEx = /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/;
+  passwordPattern = /^(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
 
   public user: any = {
     Username: "",
@@ -23,7 +24,7 @@ export class RegisterComponent implements OnInit {
     Email: ""
   };
 
-  constructor(private fb: FormBuilder, private modalService: NgbModal, public UserServices: UserServicesService, private router: Router, private activeModal: NgbActiveModal, private route: ActivatedRoute) { }
+  constructor(private fb: FormBuilder, private modalService: NgbModal, public UserServices: UserServicesService, private router: Router, private activeModal: NgbActiveModal, private route: ActivatedRoute, private toastr: ToastrService) { }
   entryComponent: [
     RegisterComponent
   ]
@@ -36,19 +37,20 @@ export class RegisterComponent implements OnInit {
     this.registerForm = this.fb.group({
       userName: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.pattern(this.emailRegEx)]],
-      phoneNumber: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      phoneNumber: ['', [Validators.required, Validators.minLength(10)]],
+      password: ['', [Validators.required, Validators.pattern(this.passwordPattern)]],
       confirmPassword: ['', [Validators.required]]
     }, {
       validator: MustMatch('password', 'confirmPassword')
     })
+    console.log(this.f.email.errors);
   }
 
   onSubmit() {
-    console.log(this.registerForm.value);
-    console.log(this.f.userName.value);
+    // console.log(this.registerForm.value);
+    // console.log(this.f.userName.value);
     console.log(this.user);
-    console.log(this.user.Username);
+    // console.log(this.user.Username);
     this.user.Username = this.f.userName.value;
     this.user.Email = this.f.email.value;
     this.user.Phone = this.f.phoneNumber.value;
@@ -61,7 +63,13 @@ export class RegisterComponent implements OnInit {
         setTimeout(() => {
           this.activeModal.dismiss();
         }, 1000);
-
+        this.toastr.success('Account Successfully', 'Success', {
+          closeButton: true,
+          tapToDismiss: true,
+          timeOut: 4000,
+          positionClass: 'toast-top-center',
+        })
+        this.registerForm.reset();
       }
 
     },
@@ -75,5 +83,7 @@ export class RegisterComponent implements OnInit {
   onReset() {
     this.registerForm.reset();
   }
+
+
 
 }
